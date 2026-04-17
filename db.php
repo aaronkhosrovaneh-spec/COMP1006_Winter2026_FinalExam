42183718a8856d6d68ed3d98e421203cea272101
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-17 12:24:21 -0400

    Created project structure and database connection

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+try {
+    $host = 'localhost';
+    $dbname = 'your_db_name';
+    $username = 'your_username';
+    $password = 'your_password';
+
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
